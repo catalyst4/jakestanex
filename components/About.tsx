e8efcbd976567f5bca1629ac4cb8ce4711e8491a
catalystfour <jake@catalystfour.co.uk>
2021-06-15 03:02:11 +0100

    about

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,7 @@ export const About = () => {
             className="mb-5 p-5 rounded-xl shadow-lg"
         >
             <h2 className="text-sm uppercase font-black tracking-widest mb-3">Mission</h2>
-            <p className="text-sm font-mono">hey im jake. i want make the world a better place. im not sure how. but i will.</p>
+            <p className="text-sm font-mono">hey im jake. i want to make the world a better place. im not sure how. but i will.</p>
         </div>
     )
 }
